bc9a9f588c5846ca81977401204c968762bd7f6b
shubh sharma <shubhsharma0333@gmail.com>
2021-04-14 22:17:42 +0530

    Finished product

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const path = require ("path");
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static(__dirname + '/public'));
-
+app.set('view engine', 'ejs');
 //storage for multer
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "files")
+    cb(null, "Files")
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())
@@ -23,19 +23,20 @@ const upload = multer({ storage: storage })
 
 //Routes
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.render("index");
+
+
 });
 
 app.post('/uploadFiles', upload.array("filesToUpload", 12), (req, res, next) => {
-  const files = req.files
-  console.log(files);
+  const files = req.files;
   if (!files) {
     const error = new Error('Please choose files')
     error.httpStatusCode = 400
     return next(error)
   }
 
-    res.send(files)
+    res.render("files", {fileItems:files});
 
 })
 
